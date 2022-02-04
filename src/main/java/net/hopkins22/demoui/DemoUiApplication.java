95100218e01b08b8466dc4e4733aa356322a0c2a
Edwin Gathura <egathura@Edwins-MacBook-Pro.local>
2022-02-04 18:28:15 -0500

    initial bootstrap and commit

---
+++ src/main/java/net/hopkins22/demoui/DemoUiApplication.java
@@ -0,0 +1,13 @@
+package net.hopkins22.demoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/hopkins22/demoui/DemoUiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hopkins22.demoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
