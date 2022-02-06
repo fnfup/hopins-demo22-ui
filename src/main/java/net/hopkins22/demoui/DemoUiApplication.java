5e956ec66ad5b702af24fe5104b8a2dc850337c1
Edwin Gathura <egathura@gmail.com>
2022-02-06 03:09:41 -0500

    instruct app to run as web controller

--- src/main/java/net/hopkins22/demoui/DemoUiApplication.java
+++ src/main/java/net/hopkins22/demoui/DemoUiApplication.java
@@ -2,12 +2,23 @@ package net.hopkins22.demoui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class DemoUiApplication {
+public class DemoUiApplication implements ErrorController  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoUiApplication.class, args);
+	}
+
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+		return "forward:/index.html";
 	}
 
 }
