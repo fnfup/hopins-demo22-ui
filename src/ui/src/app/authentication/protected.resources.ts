6c1cea60527a8dc07d57c455765561b12edb8ce6
Edwin Gathura <egathura@gmail.com>
2022-02-08 19:26:45 -0500

    tested authenticated api requests confirmed

--- src/ui/src/app/authentication/protected.resources.ts
+++ src/ui/src/app/authentication/protected.resources.ts
@@ -14,13 +14,19 @@ export type ProtectedResourceMap = Map<string, (string | ProtectedResourceScopes
 
 export type ProtectedResourceEntry = (string | ProtectedResourceScopes)[];
 
-const demoApi = "hopkins-demo22-api-sc-hopkins-demo22-api.azuremicroservices.io";
+const demoApi = "https://hopkins-demo22-api-sc-hopkins-demo22-api.azuremicroservices.io";
 const demoApiScopes: ProtectedResourceEntry = [
     "api://446ede82-f415-4e99-aed6-4d0e4ef93579/Full.Access",
     //"api://446ede82-f415-4e99-aed6-4d0e4ef93579/.default",
     //"Hopkins.Demo22.Read",
     //"Hopkins.Demo22.Order"
 ];
+
+const localApi = "http://localhost:8080";
+const localApiScopes: ProtectedResourceEntry = [
+    "api://446ede82-f415-4e99-aed6-4d0e4ef93579/Full.Access",
+];
+
 
 const azureMsGraph = "https://graph.microsoft.com/v1.0/me";
 const azureMsGraphScopes: ProtectedResourceEntry = [
@@ -29,6 +35,7 @@ const azureMsGraphScopes: ProtectedResourceEntry = [
 
 export const protectedResourceMap: ProtectedResourceMap = new Map([
     [demoApi, demoApiScopes],
-    [azureMsGraph, azureMsGraphScopes]
+    [azureMsGraph, azureMsGraphScopes],
+    [localApi, localApiScopes]
 ]);
 
